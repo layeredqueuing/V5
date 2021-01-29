de03da7811fb51fd188357f5e74a6b8791505347
Layered Queueing Network Solvers <greg@sce.carleton.ca>
2021-01-29 14:29:25 -0500

    Delete configure.

---
+++ confdefs.h
@@ -0,0 +1,21 @@
+/* confdefs.h */
+#define PACKAGE_NAME "lqns"
+#define PACKAGE_TARNAME "lqns"
+#define PACKAGE_VERSION "5.19"
+#define PACKAGE_STRING "lqns 5.19"
+#define PACKAGE_BUGREPORT "greg@sce.carleton.ca"
+#define PACKAGE_URL ""
+#define PACKAGE "lqns"
+#define VERSION "5.19"
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_DLFCN_H 1
+#define LT_OBJDIR ".libs/"
